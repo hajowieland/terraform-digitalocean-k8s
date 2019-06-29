33158266e9470efddb0b82f35da868c0a1dd06d9
hajowieland <mail@wieland.tech>
2019-06-29 22:53:50 +0200

    Refactor variables file

--- variables.tf
+++ variables.tf
@@ -1,41 +1,25 @@
 variable "enable_digitalocean" {
-  description = "Enable / Disable Digital Ocean (e.g. `1`)"
-  type        = bool
-  default     = false
-}
-
-variable "workstation_ipv4" {
-  description = "Workstation external IPv4 address"
-  type = string
-}
-
-variable "do_k8s_nodes" {
-  description = "Worker nodes (e.g. `2`)"
-  default     = 2
-}
-
-variable "do_k8s_nodepool_size" {
-  description = "Nodepool size (e.g. `3`)"
-  default = 2
+  description = "Enable / Disable Digital Ocean (e.g. `true`)"
+  default     = true
 }
 
 variable "random_cluster_suffix" {
   description = "Random 6 byte hex suffix for cluster name"
-  type = string
+  type        = string
+  default     = ""
 }
 
 variable "do_token" {
-  description = "Digital Ocean Access token"
+  description = "Digital Ocean Personal access token"
   type        = string
   default     = "DUMMY"
 }
 
 variable "do_region" {
-  description = "Digital Ocean Region to use (e.g. `fra1` => Frankfurt)"
+  description = "Digital Ocean region (e.g. `fra1` => Frankfurt)"
   type        = string
   default     = "fra1"
 }
-
 
 variable "do_k8s_name" {
   description = "Digital Ocean Kubernetes cluster name (e.g. `k8s-do`)"
@@ -43,32 +27,36 @@ variable "do_k8s_name" {
   default     = "k8s-do"
 }
 
-variable "do_k8s_version" {
-  description = "Digital Ocean Kubernetes version to use (e.g. `1.14.2-do.0`)"
+variable "do_k8s_pool_name" {
+  description = "Digital Ocean Kubernetes default node pool name (e.g. `k8s-do-nodepool`)"
   type        = string
-  default     = "1.14.2-do.0"
+  default     = "k8s-mainpool"
 }
 
-# variable "do_k8s_node_count" {
-#   description = "Digital Ocean Kubernets nodes (e.g. `2`)"
-#   type        = string
-#   default     = 2
-# }
+variable "do_k8s_nodes" {
+  description = "Digital Ocean Kubernetes default node pool size (e.g. `2`)"
+  default     = 1
+}
 
 variable "do_k8s_node_type" {
-  description = "Digital Ocean Kubernetse Node Plan (e.g. `s-1vcpu-2gb` => 1vCPU, 2GB RAM)"
+  description = "Digital Ocean Kubernetes default node pool type (e.g. `s-1vcpu-2gb` => 1vCPU, 2GB RAM)"
   type        = string
   default     = "s-1vcpu-2gb"
 }
 
-variable "do_k8s_nodepool_type" {
-  description = "Digital Ocean Kubernetes additional Node Pool Type (e.g. `s-1vcpu-2gb` => 1vCPU, 2GB RAM)"
-  type = string
-  default = "s-1vcpu-2gb"
+variable "do_k8s_nodepool_name" {
+  description = "Digital Ocean Kubernetes additional node pool name (e.g. `k8s-do-nodepool`)"
+  type        = string
+  default     = "k8s-nodepool"
 }
 
-variable "do_k8s_pool_name" {
-  description = "Digital Ocean Kubernetse Node Pool name (e.g. `k8s-do-nodepool`)"
+variable "do_k8s_nodepool_type" {
+  description = "Digital Ocean Kubernetes additional node pool type (e.g. `s-1vcpu-2gb` => 1vCPU, 2GB RAM)"
   type        = string
-  default     = "dev-cluster-nodepool-do"
+  default     = "s-1vcpu-2gb"
+}
+
+variable "do_k8s_nodepool_size" {
+  description = "Digital Ocean Kubernetes additional node pool size (e.g. `3`)"
+  default     = 2
 }
